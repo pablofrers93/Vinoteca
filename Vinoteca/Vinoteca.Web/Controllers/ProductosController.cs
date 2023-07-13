11c669f323465601c08d7507d8d1ebb9f2b541a4
pablofrers93 <pablofrers93@gmail.com>
2023-07-12 22:15:12 -0300

    listando productos

---
+++ Vinoteca/Vinoteca.Web/Controllers/ProductosController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vinoteca.Servicios.Interfaces;
+using Vinoteca.Web.App_Start;
+
+namespace Vinoteca.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        private readonly IServiciosProductos _servicios;
+        private readonly IServiciosBodegas _serviciosBodegas;
+        private readonly IServiciosVariedades _serviciosVariedades;
+        private readonly IServiciosTipoProductos _serviciosTipoProductos;
+        private readonly IMapper _mapper;
+        public ProductosController(IServiciosProductos servicios,
+            IServiciosBodegas serviciosBodegas,
+            IServiciosVariedades serviciosVariedades,
+            IServiciosTipoProductos serviciosTipoProductos)
+        {
+            _servicios = servicios;
+            _serviciosBodegas = serviciosBodegas;
+            _serviciosVariedades = serviciosVariedades;
+            _serviciosTipoProductos = serviciosTipoProductos;
+            _mapper = AutoMapperConfig.Mapper;
+        }
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var lista = _servicios.GetProductos();
+            var listaVm = _mapper.Map<List<ProductoListVm>>(lista);
+            return View(listaVm);
+        }
+    }
+}
---
+++ Vinoteca/Vinoteca.Web/ViewModels/Producto/ProductoListVm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Vinoteca.Web.ViewModels.Producto
+{
+    public int ProductoId { get; set; }
+    [DisplayName("Producto")]
+    public string NombreProducto { get; set; }
+    [DisplayName("Categoría")]
+    public string Categoria { get; set; }
+
+    [DisplayName("P. Unit")]
+    public decimal PrecioUnitario { get; set; }
+    [DisplayName("Stock")]
+    public int UnidadesDisponibles { get; set; }
+    public bool Suspendido { get; set; }
+    [DisplayName("Imágen")]
+    public string Imagen { get; set; }
+}
